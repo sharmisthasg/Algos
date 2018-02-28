9b86ca68698174a883a29261a6a11a58127415fe
Sharmistha S. Gupta <sharmistha16193@iiitd.ac.in>
2018-03-01 03:56:47 +0530

    Update insertion_sort.js

--- insertion_sort.js
+++ insertion_sort.js
@@ -3,16 +3,20 @@ var array = prompt("Enter your numbers").split(",");
 
 function insertionsort(array){
 	
+	//Traversing the entire length of the array
 	for (var i = 0; i < array.length; i++){
 		var cur = array[i];
 		var pos = i;
+		//Moving elements before the current position, 
+		//and greater than the current element,
+		//to one position ahead of their current position
 		while (cur < array[pos-1] && pos>=1){
 			array[pos] = array[pos-1];
 			pos = pos - 1;
 		}
 		array[pos] = cur;	
 	}
-	return array;
+	return array; //array is now sorted
 }
 
 document.write(insertionsort(array));
